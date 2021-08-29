6d074fbb67481c55fdb89b7ebfe3c0aebb42e546
Aldy Ardiansyah <aldyardiansyah628@gmail.com>
2021-08-29 11:53:29 +0700

    update database

--- pegawai/db_android.sql
+++ pegawai/db_android.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 16, 2019 at 02:39 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Waktu pembuatan: 29 Agu 2021 pada 06.37
+-- Versi server: 10.4.18-MariaDB
+-- Versi PHP: 7.3.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,21 +24,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_pegawai`
+-- Struktur dari tabel `tb_pegawai`
 --
 
 CREATE TABLE `tb_pegawai` (
   `id` int(11) NOT NULL,
   `nama` varchar(30) DEFAULT NULL,
   `posisi` varchar(20) DEFAULT NULL,
-  `gaji` int(11) DEFAULT NULL
+  `gajih` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_pegawai`
+-- Dumping data untuk tabel `tb_pegawai`
 --
 
-INSERT INTO `tb_pegawai` (`id`, `nama`, `posisi`, `gaji`) VALUES
+INSERT INTO `tb_pegawai` (`id`, `nama`, `posisi`, `gajih`) VALUES
 (1, 'Komang', 'Programmer', 2000),
 (2, 'Medi', 'Programmer', 1000),
 (3, 'Fina', 'Staff', 500);
@@ -47,20 +48,22 @@ INSERT INTO `tb_pegawai` (`id`, `nama`, `posisi`, `gaji`) VALUES
 --
 
 --
--- Indexes for table `tb_pegawai`
+-- Indeks untuk tabel `tb_pegawai`
 --
 ALTER TABLE `tb_pegawai`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `tb_pegawai`
+-- AUTO_INCREMENT untuk tabel `tb_pegawai`
 --
 ALTER TABLE `tb_pegawai`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
